0518ae457c4e51695a183f51c3b00f8a1e194235
Daniel Facelli García <facellidaniel@gmail.com>
2025-09-21 03:15:36 +0000

    La pantalla de insumos no debería de tener material de insumos, solament

--- src/lib/defaults.ts
+++ src/lib/defaults.ts
@@ -15,16 +15,7 @@ export const DEFAULT_SETTINGS: Settings = {
 };
 
 export const DEFAULT_MATERIALS: Material[] = [
-  { id: 'pla_default_1', name: 'PLA Estándar Blanco', type: 'PLA', cost: 20 },
-  { id: 'pla_default_2', name: 'PLA Silk Dorado', type: 'PLA', cost: 28 },
-  { id: 'abs_default', name: 'ABS Gris', type: 'ABS', cost: 22 },
-  { id: 'petg_default', name: 'PETG Estándar Negro', type: 'PETG', cost: 25 },
-  { id: 'tpu_default', name: 'TPU Flexible Rojo', type: 'TPU/TPE', cost: 35 },
-  { id: 'nylon_default', name: 'Nylon Natural', type: 'Nylon', cost: 40 },
-  { id: 'asa_default', name: 'ASA Negro Exterior', type: 'ASA', cost: 38 },
-  { id: 'pc_default', name: 'Policarbonato Transparente', type: 'PC', cost: 45 },
-  { id: 'wood_default', name: 'PLA Filamento Madera', type: 'Wood', cost: 33 },
-  { id: 'carbon_default', name: 'PETG Fibra de Carbono', type: 'CarbonFiber', cost: 55 },
+  { id: 'pla_default_black', name: 'PLA Estándar Negro', type: 'PLA', cost: 20 },
 ];
 
 export const DEFAULT_MACHINES: Machine[] = [
